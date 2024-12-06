dd865cb4afe3d4984c56229c1f851f71bbf35a6e
Kyron <tfkyron@gmail.com>
2024-12-06 02:40:48 +0000

    Feat: Setup typhography, variables and breakpoints

--- wp-content/themes/starter-template/functions/theme-setup.php
+++ wp-content/themes/starter-template/functions/theme-setup.php
@@ -14,11 +14,11 @@ function admin_styles() {
 }
 add_action('admin_enqueue_scripts', 'admin_styles');
 
-// Enqueue theme scripts
-function theme_scripts() {
-    wp_enqueue_script('theme-script', get_template_directory_uri() . '/dist/js/main.min.js', [], THEME_VERSION, true);
+// Enqueue theme fonts
+function enqueue_google_fonts() {
+    wp_enqueue_style('google-fonts', 'https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@400;700&display=swap', false);
 }
-add_action('wp_enqueue_scripts', 'theme_scripts');
+add_action('wp_enqueue_scripts', 'enqueue_google_fonts');
 
 // Disable gutenburg default blocks
 add_filter('allowed_block_types', 'disable_default_blocks', 10, 2);
--- wp-content/themes/starter-template/index.php
+++ wp-content/themes/starter-template/index.php
@@ -4,6 +4,14 @@ get_header();
 
 <main>
     <section>
+        <h1><?php the_title(); ?></h1>
+        <h2><?php the_title(); ?></h2>
+        <h3><?php the_title(); ?></h3>
+        <h4><?php the_title(); ?></h4>
+        <h5><?php the_title(); ?></h5>
+        <h6><?php the_title(); ?></h6>
+        <p><?php the_title(); ?></p>
+        <p><?php the_title(); ?></p>
         <?php the_content(); ?>
     </section>
 </main>
