1b878c9e07c9b4eaf65d3a9a47797f187a2077c1
Kyron <tfkyron@gmail.com>
2024-12-08 01:19:13 +0000

    Feat: Initial mega menu build

--- wp-content/themes/starter-template/template-parts/layout/main-menu.php
+++ wp-content/themes/starter-template/template-parts/layout/main-menu.php
@@ -1,12 +1,13 @@
 <nav class="main-menu">
     <?php
     wp_nav_menu([
-        'theme_location' => 'main-menu',
-        'container' => false,
-        'menu_class' => 'menu',
-        'menu_id' => 'main-menu',
-        'depth' => 2,
-        'fallback_cb' => 'wp_page_menu',
+    'theme_location' => 'main-menu',
+    'container' => false,
+    'menu_class' => 'menu',
+    'menu_id' => 'main-menu',
+    'depth' => 2,
+    'fallback_cb' => 'wp_page_menu',
+    'walker' => new Custom_Walker_Nav_Menu(),
     ]);
     ?>
 </nav>
