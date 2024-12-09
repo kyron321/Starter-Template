5673459b16d851dde3d41c4e38083aeccbd33570
Kyron <tfkyron@gmail.com>
2024-12-09 00:29:58 +0000

    Feat: Basic Banner styling

--- wp-content/themes/starter-template/blocks/banner.php
+++ wp-content/themes/starter-template/blocks/banner.php
@@ -2,29 +2,33 @@
 // Retrieve ACF fields
 $banner_image = get_field('banner_image');
 $banner_heading = get_field('banner_heading');
+$banner_copy = get_field('banner_copy');
 $banner_link = get_field('banner_link');
 
 // Retrieve cloned block settings
 $block_settings = get_field('block_settings');
-
-// Get the CSS classes
 $block_classes = get_block_classes($block_settings);
 
-// Display the banner
 if ($banner_image || $banner_heading || $banner_link) : ?>
-    <div class="banner <?php echo esc_attr($block_classes); ?>">
-        <?php if ($banner_image) : ?>
-            <img src="<?php echo esc_url($banner_image['url']); ?>" alt="<?php echo esc_attr($banner_image['alt']); ?>" />
+<div class="banner <?php echo esc_attr($block_classes); ?>">
+    <?php if ($banner_image) : ?>
+    <img src="<?php echo esc_url($banner_image['url']); ?>" alt="<?php echo esc_attr($banner_image['alt']); ?>" />
+    <?php endif; ?>
+
+    <div class="text-container">
+        <?php if ($banner_heading) : ?>
+        <h1><?php echo esc_html($banner_heading); ?></h1>
         <?php endif; ?>
 
-        <?php if ($banner_heading) : ?>
-            <h2><?php echo esc_html($banner_heading); ?></h2>
+        <?php if ($banner_copy) : ?>
+        <?php echo $banner_copy; ?>
         <?php endif; ?>
 
         <?php if ($banner_link) : ?>
-            <a href="<?php echo esc_url($banner_link['url']); ?>" target="<?php echo esc_attr($banner_link['target']); ?>">
-                <?php echo esc_html($banner_link['title']); ?>
-            </a>
-        <?php endif; ?>
+        <a href="<?php echo esc_url($banner_link['url']); ?>" target="<?php echo esc_attr($banner_link['target']); ?>">
+            <?php echo esc_html($banner_link['title']); ?>
+        </a>
     </div>
+    <?php endif; ?>
+</div>
 <?php endif; ?>
